afc852ad49713a9c936f8c3a5c0858043654b293
praveenreddysheelam <41960688+praveenreddysheelam@users.noreply.github.com>
2022-05-26 20:19:01 -0500

    Add files via upload

---
+++ OFS/public/app.js
@@ -0,0 +1,71 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var flash  = require("connect-flash");
+const session = require('express-session');
+var methodOverride=require('method-override');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var videosRouter = require('./routes/videos');
+var productsRouter = require('./routes/products');
+var app = express();
+var http = require('http');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+require('./config/passport')(passport);
+mongoose.connect('mongodb://localhost:27017/OFS');
+var LocalStrategy = require('passport-local').Strategy;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/videos', videosRouter);
+app.use('/products',productsRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
